3f8113eb62b4ea832b7badc2f7ba64970abd01a6
eXe-Kricket <daimio2008@yandex.ru>
2026-01-14 11:32:06 +0500

    test

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -4,12 +4,6 @@
 go mod init prices-server
 go mod tidy
 
-# Предполагаем, что PostgreSQL установлен локально или через Docker
-# Запускаем PostgreSQL через Docker, если не запущен
-if ! docker ps | grep -q postgres; then
-    docker run --name postgres-db -e POSTGRES_PASSWORD=val1dat0r -e POSTGRES_USER=validator -e POSTGRES_DB=project-sem-1 -p 5432:5432 -d postgres
-fi
-
 # Ждем, пока БД запустится
 sleep 5
 
